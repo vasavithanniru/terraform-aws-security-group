e7a8413bab0499c293914abf8c63c7d61a4fb8cb
vasavi <tvasu370@gmail.com>
2024-10-03 08:25:54 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "common_tags" {
+    default = {    }
+}
+
+variable "sg_tags" {
+  default = {}
+}
+
+variable "sg_name" {
+  
+}
