8550144bf4751776d6d018e1c911a08e3aa16af6
vasavi <tvasu370@gmail.com>
2024-10-09 09:24:39 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-resource "aws_security_group" "main"{
-    name = local.sg_final_name
-    description = "allow TLS inbound raffic and all outbound traffic"
-    vpc_id = var.vpc_id
+resource "aws_security_group" "main" {
+  name        = local.sg_final_name
+  description = "allow TLS inbound raffic and all outbound traffic"
+  vpc_id      = var.vpc_id
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    var.sg_tags,
+    {
+      Name = local.sg_final_name
     }
-
-    tags = merge(
-        var.common_tags,
-        var.sg_tags,
-        {
-            Name = local.sg_final_name
-        }
-    )
+  )
 }
